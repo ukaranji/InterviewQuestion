ae80ec55e28b91896ad9e77020120df93554703b
ukaranji <utsav.karanjit@alaskaair.com>
2024-10-01 07:27:53 -0700

    Updated bareBones

--- interview-react/src/App.jsx
+++ interview-react/src/App.jsx
@@ -2,21 +2,7 @@ import { useState } from 'react';
 import './styles.scss';
 
 function App() {
-  const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    dob: '',
-    phoneNumber: '',
-    username: '',
-    password: ''
-  });
-
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
   };
 
   const handleSubmit = (e) => {
@@ -30,27 +16,27 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Firstname</label>
-          <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} />
+          <input type="text" name="firstname" onChange={handleChange} />
         </div>
         <div>
           <label>Lastname</label>
-          <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} />
+          <input type="text" name="lastname"  onChange={handleChange} />
         </div>
         <div>
           <label>Date of Birth</label>
-          <input type="date" name="dob" value={formData.dob} onChange={handleChange} />
+          <input type="date" name="dob"onChange={handleChange} />
         </div>
         <div>
           <label>Phone Number</label>
-          <input type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+          <input type="tel" name="phoneNumber" onChange={handleChange} />
         </div>
         <div>
           <label>Username</label>
-          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+          <input type="text" name="username" onChange={handleChange} />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+          <input type="password" name="password"  onChange={handleChange} />
         </div>
         <button type="submit">Submit</button>
       </form>
