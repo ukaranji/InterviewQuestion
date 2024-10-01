b4767a3c07895a8d876c4e17ae33ccc9442d4f56
ukaranji <utsav.karanjit@alaskaair.com>
2024-10-01 07:55:03 -0700

    Refactored

--- interview-react/src/App.jsx
+++ interview-react/src/App.jsx
@@ -1,45 +1,10 @@
-import { useState } from 'react';
-import './styles.scss';
+import React from 'react';
+import FormComponent from './Signup/FormComponent';
 
 function App() {
-  const handleChange = (e) => {
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form Data Submitted:', formData);
-  };
-
   return (
     <div className="App">
-      <h1>Interview Application Form</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Firstname</label>
-          <input type="text" name="firstname" onChange={handleChange} />
-        </div>
-        <div>
-          <label>Lastname</label>
-          <input type="text" name="lastname"  onChange={handleChange} />
-        </div>
-        <div>
-          <label>Date of Birth</label>
-          <input type="date" name="dob"onChange={handleChange} />
-        </div>
-        <div>
-          <label>Phone Number</label>
-          <input type="tel" name="phoneNumber" onChange={handleChange} />
-        </div>
-        <div>
-          <label>Username</label>
-          <input type="text" name="username" onChange={handleChange} />
-        </div>
-        <div>
-          <label>Password</label>
-          <input type="password" name="password"  onChange={handleChange} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      <FormComponent />
     </div>
   );
 }
---
+++ interview-react/src/Signup/FormComponent.jsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './styles.scss';
+
+const FormComponent = () => {
+  const [formData, setFormData] = useState({
+    firstname: '',
+    lastname: '',
+    dob: '',
+    phoneNumber: '',
+    username: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Data Submitted:', formData);
+  };
+
+  return (
+    <div className="Form">
+      <h1>Interview Application Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Firstname</label>
+          <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Lastname</label>
+          <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Date of Birth</label>
+          <input type="date" name="dob" value={formData.dob} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Phone Number</label>
+          <input type="tel" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Username</label>
+          <input type="text" name="username" value={formData.username} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default FormComponent;
